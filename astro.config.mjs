b88f65900be18a8d2cfccf48dd402c4682a6c9c0
rarifbkn <arif.bordones@gmail.com>
2025-02-11 23:44:53 -0300

    feat:(astro islands - slug pages) add theme toggle and slug pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://rarifbkn.netlify.app/"
+  site:"https://rarifbkn.netlify.app/",
+  integrations: [preact()]
 });
